32b27ff62b84615b461708916d46090ecc5a1679
QiZhang <jason.qizhang.b@gmail.com>
2020-06-29 20:41:02 +0800

    Finished cateList page

---
+++ src/mixins/tabs/cates.js
@@ -0,0 +1,39 @@
+import wepy from '@wepy/core'
+
+export default {
+  data: {
+    cateDataList: [],
+    activeKey: 0,
+    windowHeight: 0,
+    secondCateList: []
+  },
+  onLoad() {
+    this.getCateDataList()
+    this.getWindowHeight()
+  },
+  methods: {
+    async getCateDataList() {
+      const { data: res } = await wepy.get('/categories')
+      if (res.meta.status !== 200) return wepy.baseToast(res.meta.msg)
+      this.cateDataList = res.message
+      this.secondCateList = this.cateDataList[0].children
+      console.log(this.secondCateList)
+    },
+    onChange(e) {
+      console.log(e.$wx.detail)
+      this.secondCateList = this.cateDataList[e.$wx.detail].children
+      console.log(this.secondCateList)
+    },
+    async getWindowHeight() {
+      const info = await wepy.wx.getSystemInfo()
+      console.log(info)
+      if (info.errMsg !== 'getSystemInfo:ok') return
+      this.windowHeight = info.windowHeight
+    },
+    goGoodsList(cateId) {
+      wepy.wx.navigateTo({
+        url: '/pages/goods_list?cateId=' + cateId
+      })
+    }
+  }
+}
