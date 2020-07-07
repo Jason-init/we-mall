412cc0ca8b9abb6f981d698ebcf11323ab3a6526
QiZhang <jason.qizhang.b@gmail.com>
2020-07-07 21:07:56 +0800

    Finished order page

--- src/mixins/order.js
+++ src/mixins/order.js
@@ -1,6 +1,33 @@
 import wepy from '@wepy/core'
+import { mapState } from '@wepy/x'
 
 export default {
-  data: {},
-  methods: {}
+  data: {
+    address: null,
+    checkedGoods: []
+  },
+  methods: {
+    async chooseAddress() {
+      const res = await wepy.wx.chooseAddress().catch(err => err)
+      if (res.errMsg !== 'chooseAddress:ok') return
+      this.address = res
+      wepy.wx.setStorageSync('address', res)
+    }
+  },
+  onLoad() {
+    this.address = wepy.wx.getStorageSync('address') || null
+    this.checkedGoods = this.cart.filter(item => {
+      return item.isChecked === true
+    })
+  },
+  computed: {
+    ...mapState(['cart']),
+    hasSavedAddress() {
+      return this.address !== null
+    },
+    addressString() {
+      if (this.address === null) return ''
+      return this.address.provinceName + this.address.cityName + this.address.countyName + this.address.detailInfo
+    }
+  }
 }
